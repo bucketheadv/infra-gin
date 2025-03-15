45f19a8685e048910633811a3daf596c879bbb10
liuqinglin <liuqinglin@wekoi.cn>
2025-03-15 17:33:30 +0800

    Engine强制注册ErrorHandler

--- engine.go
+++ engine.go
@@ -1,6 +1,7 @@
 package infra_gin
 
 import (
+	"github.com/bucketheadv/infra-gin/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,4 +9,5 @@ var Engine *gin.Engine
 
 func init() {
 	Engine = gin.Default()
+	middlewares.RegErrorHandler(Engine)
 }
